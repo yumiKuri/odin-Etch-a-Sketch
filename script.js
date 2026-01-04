a9518752d224e5ced1c9790aef675d8b79e2f09f
yumiKuri <pianosong2016@gmail.com>
2026-01-04 20:41:50 +0700

    Create a grid generator function

---
+++ script.js
@@ -0,0 +1,21 @@
+const screen = document.getElementById("screen");
+const input = document.getElementById("gridnNum");
+const resizeBtn = document.getElementById("resizeBtn");
+
+createGrid(16);
+
+function createGrid(n){
+    n = Number(n);
+    if(!Number.isInteger(n) || n <= 0) return;
+
+    screen.innerHTML = "";
+
+    for(let i = 0; i < n*n; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cells");
+        screen.appendChild(cell);
+    }
+
+    screen.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    screen.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+}
